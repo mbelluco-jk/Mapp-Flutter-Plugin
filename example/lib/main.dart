56dc31bf0700647bbaf4b7c1dc084f3ff1b8897e
Stefan <stefan.stevanovic@mapp.com>
2021-11-07 15:35:19 +0100

    example app created

---
+++ example/lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:plugin_codelab/plugin_codelab.dart';
+
+//import '../../lib/plugin_codelab.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: const Color(0xFF00BAFF),
+          primaryColorDark: const Color(0xFF0592D7),
+          accentColor: const Color(0xFF58585A),
+          cardColor: const Color(0xFF888888)),
+      home: HomePage(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _platformVersion = 'Unknown';
+  List<String> _screens = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String? platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await PluginCodelab.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+      _screens = [
+        "Set Device Alias Text",
+        "Set Device Alias",
+        "Get Device Alias",
+        "Device Information",
+        "Is Push Enabled",
+        "Opt in",
+        "Opt out",
+        "Start Geo",
+        "Stop Geo",
+        "Fetch inbox messages",
+        "In App: App Open",
+        "In App: App Feedback",
+        "In App: App Discount",
+        "In App: App Promo",
+        "Get Tags",
+        "Set Tags",
+        "Remove Tag",
+        "Set Attribute",
+        "Get Attribute",
+        "Remove Attribute",
+        "Remove Badge Number",
+        "Lock Orientation",
+        "Engage",
+        "Log out"
+      ];
+    });
+  }
+
+  void _determineWidget(int index) {
+    // switch (index) {
+    //   case 0:
+    //     return DetailsView(index);
+    //   case 1:
+    //     return PageTracking();
+    //   case 2:
+    //     return ActionTracking();
+    //   case 3:
+    //     return Campaign();
+    //   case 4:
+    //     return Ecommerce();
+    //   case 5:
+    //     return WebviewApp();
+    //   case 6:
+    //     return WebviewForAndroid();
+    //   case 7:
+    //     return Media();
+    //   default:
+    //     return DetailsView(index);
+    // }
+  }
+
+  Card _createTextFieldOrButton(int index) {
+    switch (index) {
+      case 0:
+        return Card(
+          child: TextFormField(
+            decoration: const InputDecoration(
+                border: UnderlineInputBorder(), labelText: 'Enter alias'),
+          ),
+        );
+      default:
+        return Card(
+          child: ListTile(
+            title: Text(
+              _screens[index],
+              style: TextStyle(color: Theme.of(context).primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            onTap: () {},
+          ),
+        );
+    }
+  }
+
+  ListView _buildListView(BuildContext context) {
+    return ListView.builder(
+      itemCount: _screens.length,
+      itemBuilder: (context, index) {
+        return _createTextFieldOrButton(index);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Mapp SDK Demo'),
+            backgroundColor: Theme.of(context).primaryColorDark,
+          ),
+          body: _buildListView(context)),
+    );
+  }
+}
