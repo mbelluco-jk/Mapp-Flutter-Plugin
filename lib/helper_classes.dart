7bffbfd6779698b6e21064dc0389a83dd3bb001e
Stefan <stefan.stevanovic@mapp.com>
2022-01-21 12:05:11 +0100

    styles update

--- lib/helper_classes.dart
+++ lib/helper_classes.dart
@@ -1,12 +1,17 @@
 enum SERVER { L3, L3_US, EMC, EMC_US, CROC, TEST, TEST55 }
 
-class Config{
-  static const String sdkKey="17e243d300361d.38133363";
-  static const String appID="206846";
-  static const String googleProjectId="785651527831";
-  static const String tenantID="5963";
-  static const server=SERVER.L3;
+class Config {
+  static const String sdkKey = "17e243d300361d.38133363";
+  static const String appID = "206846";
+  static const String googleProjectId = "785651527831";
+  static const String tenantID = "5963";
+  static const server = SERVER.EMC;
 
-  static final defaultConfig=[Config.sdkKey, Config.googleProjectId, Config.server, Config.appID, Config.tenantID];
+  static final defaultConfig = [
+    Config.sdkKey,
+    Config.googleProjectId,
+    Config.server,
+    Config.appID,
+    Config.tenantID
+  ];
 }
-
